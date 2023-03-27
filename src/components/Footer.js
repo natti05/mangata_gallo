72c02c99d79f06814bf5b232b69c70f464154656
Natalia Takahara <takaharanatalia@gmail.com>
2023-03-27 15:54:05 +0200

    nav and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import logo from './logos/m.g3.png';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <img src={logo} className="Logo-footer" alt="logo" />
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import logo from './logos/m.g4.png';
+import './Nav.css';
+
+function Nav() {
+  return (
+  <section>
+    <header>
+      <img src={logo} className="Head-logo" alt="logo" />
+    </header>
+    <nav>
+
+    </nav>
+  </section>
+    
+  );
+}
+
+export default Nav;
