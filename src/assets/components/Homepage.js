26e9e5e6349b377a4b2e4b259f0deb625de80f0e
Natalia Takahara <takaharanatalia@gmail.com>
2023-05-27 02:17:16 +0200

    added text to image homepage

--- src/assets/components/Homepage.js
+++ src/assets/components/Homepage.js
@@ -3,11 +3,12 @@ function Homepage() {
     return (
         <div>
             <div className='picsix' >
+            <img src={pic6} className="" alt="pic6" />
                 <h1>Lorem ipsum dolor sit amet,<br/>
-                     consectetur adipiscing elit, <br/>
-                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h1>
-                     <p>Lorem ipsum</p>
-                <img src={pic6} className="" alt="pic6" />
+                    consectetur adipiscing elit, <br/>
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </h1>
+                <p>Lorem ipsum</p>
+               
             </div>
 
         </div>
