9f4a8a5cf97d30e5cf203f97502465503505a6cb
Natalia Takahara <takaharanatalia@gmail.com>
2023-05-28 23:51:41 +0200

    feedback added

--- src/assets/components/Contact.js
+++ src/assets/components/Contact.js
@@ -1,9 +1,10 @@
+import Feedback from '../components/Feedback';
+
 function Contact() {
     return (
         <div>
             <div>
-               <h1>Give us a feedback</h1>
-            
+              <Feedback/>
             </div>
             <div>
             <p>Our address: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
