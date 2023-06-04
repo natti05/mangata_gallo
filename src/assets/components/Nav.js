63de5a9dd2519dd9dd54a625c52d414ecabd7a4e
Natalia Takahara <takaharanatalia@gmail.com>
2023-06-05 00:42:47 +0200

    change pictures and added changes at json file

--- src/assets/components/Nav.js
+++ src/assets/components/Nav.js
@@ -3,9 +3,42 @@ import '../assets/../css/FooterNav.css';
 import Homepage from '../components/Homepage';
 import About from '../components/About';
 import Contact from '../components/Contact';
-// import Card from '../components/Cards';
 import React,{useState,useEffect} from 'react';
 import { Routes, Route, Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+  <section>
+    <header>
+      <Link to="/">
+      <div className='logo' >
+         <img src={logo} className="head-logo" alt="logo" />
+      </div>
+      </Link>
+      <nav>
+        <Link to="/bestSellers" className="nav-item">BestSellers</Link>
+        <Link to="/earrings" className="nav-item">Earrings</Link>
+        <Link to="/bracelets" className="nav-item">Bracelets</Link>
+        <Link to="/necklaces" className="nav-item">Necklaces</Link>
+        <Link to="/rings" className="nav-item">Rings</Link>
+        <Link to="/about" className="nav-item">About us</Link>
+        <Link to="/contact" className="nav-item">Contact </Link>
+      </nav>
+    </header>
+      <Routes> 
+        <Route path="/bestSellers" element={<BestSellers />} />
+        <Route path="/earrings" element={<Earrings />} />
+        <Route path="/bracelets" element={<Bracelets/>} />
+        <Route path="/necklaces" element={<Necklaces />} />
+        <Route path="/rings" element={<Rings />} />
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+  </section>
+    
+  );
+}
 
   const BestSellers = () =>{
     const [data,setData]=useState([]);
@@ -188,39 +221,3 @@ function Rings() {
     </div>
   );
 }
-
-function Nav() {
-  return (
-  <section>
-    <header>
-      <Link to="/">
-      <div className='logo' >
-         <img src={logo} className="head-logo" alt="logo" />
-      </div>
-      </Link>
-      <nav>
-        <Link to="/bestSellers" className="nav-item">BestSellers</Link>
-        <Link to="/earrings" className="nav-item">Earrings</Link>
-        <Link to="/bracelets" className="nav-item">Bracelets</Link>
-        <Link to="/necklaces" className="nav-item">Necklaces</Link>
-        <Link to="/rings" className="nav-item">Rings</Link>
-        <Link to="/about" className="nav-item">About us</Link>
-        <Link to="/contact" className="nav-item">Contact </Link>
-      </nav>
-    </header>
-      <Routes> 
-        <Route path="/bestSellers" element={<BestSellers />} />
-        <Route path="/earrings" element={<Earrings />} />
-        <Route path="/bracelets" element={<Bracelets/>} />
-        <Route path="/necklaces" element={<Necklaces />} />
-        <Route path="/rings" element={<Rings />} />
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
-  </section>
-    
-  );
-}
-
-export default Nav;
